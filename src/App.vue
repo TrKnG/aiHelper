2734a38746d5f97d55d3961a95a3f3d217cfb412
Enes Malik <etok6181@gmail.com>
2025-04-29 20:21:13 +0300

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,70 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="chatbot-page">
+    <div class="content">
+      <h1>Merhaba <span class="wave">👋</span></h1>
+      <p>Yardımcı olmamı ister misin? Sağ alttaki sohbet butonuna tıklayabilirsin.</p>
+      <img
+        src="https://cdn-icons-png.flaticon.com/512/4712/4712109.png"
+        alt="Chatbot Illustration"
+      />
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
+<script type="module">
+    import Chatbot from "https://cdn.jsdelivr.net/npm/flowise-embed/dist/web.js"
+    Chatbot.init({
+        chatflowid: "133220bb-f71a-4953-ac20-9a50ff99e3cb",
+        apiHost: "https://yz.ulakbel.com",
+    })
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
+.chatbot-page {
+
+  min-height: 100vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.content {
+  text-align: center;
+  max-width: 800px;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+h1 {
+  font-size: 4rem;
+  color: #c9c9c9;
+  margin-bottom: 1rem;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+p {
+  font-size: 1.5rem;
+  color: #666;
+  margin-bottom: 2rem;
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+img {
+  width: 300px;
+  max-width: 100%;
+  opacity: 0.95;
+}
+
+.wave {
+  display: inline-block;
+  animation: wave 1.5s infinite;
+}
+
+@keyframes wave {
+  0% { transform: rotate(0deg); }
+  15% { transform: rotate(14deg); }
+  30% { transform: rotate(-8deg); }
+  45% { transform: rotate(14deg); }
+  60% { transform: rotate(-4deg); }
+  75% { transform: rotate(10deg); }
+  100% { transform: rotate(0deg); }
 }
 </style>
